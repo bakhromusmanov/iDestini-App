d84dd09e61cf80fe92ccdb4e5fde6242ccfd3c8f
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-07-07 17:26:37 +0500

    Initial Commit

---
+++ iDestini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iDestini
+//
+//  Created by Bakhrom Usmanov on 07/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
